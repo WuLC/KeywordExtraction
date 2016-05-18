a90368912b2d743ecf53e9cdb2bedb4914fed78f
WuLC <liangchaowu5@gmail.com>
2016-05-18 23:32:07 +0800

    TextRank implementation,allow to change number of keywords and size of co-occur window

---
+++ README.md
@@ -0,0 +1,34 @@
+Keyword Extraction in Java
+
+Serveral implementations of keyword extraction,including TextRank,TF-IDF,TextRank along with TFTF-IDF
+
+Segment words and filter stop words with [HanLP](https://github.com/hankcs/HanLP)
+
+## Algorithm
+
+## TextRank
+
+With file name and content as input,return 5 keywords extracted by TextRank.For example
+
+```java
+String title = "关键词抽取";
+String content = "关键词自动提取是一种识别有意义且具有代表性片段或词汇的自动化技术。关键词自动提取在文本挖掘域被称为关键词抽取，在计算语言学领域通常着眼于术语自动识别，在信息检索领域，就是指自动标引。";
+System.out.println(TextRank.getKeyword(title, content));
+```
+
+Output: `[自动, 领域, 关键词, 提取, 抽取]`
+
+You can change keywords number and the size of co-occur window ,whose default values are 5 and 3,respectively.For example:
+```java
+TextRank.setKeywordNumber(6);
+TextRank.setWindowSize(4);
+String titleTest = "关键词抽取";
+String contentTest = "关键词自动提取是一种识别有意义且具有代表性片段或词汇的自动化技术。关键词自动提取在文本挖掘域被称为关键词抽取，在计算语言学领域通常着眼于术语自动识别，在信息检索领域，就是指自动标引。";
+System.out.println(TextRank.getKeyword(titleTest, contentTest));
+```
+
+Output:`[自动, 关键词, 领域, 提取, 抽取, 自动识别]`
+
+From the output you can see clearly the number of keywords has change due to `TextRank.setKeywordNumber(6);`,and the size of co-occur window is not visible in the result but will affect the resutl if you are aware of the principle of TextRank algorithm.
+
+
--- src/com/lc/nlp/keyword/algorithm/TextRank.java
+++ src/com/lc/nlp/keyword/algorithm/TextRank.java
@@ -1,39 +1,52 @@
+package com.lc.nlp.keyword.algorithm;
+
 /**
- * 通过TextRank提取文本关键词，输入String格式的文本 ，输出List<String>格式的关键字
+ * Function: extract keywords of document through TextRank algorithm
+ * Input(String): target text that keywords will be extracted from 
+ * Output(List<String>): keywords of the text
  */
-package com.lc.nlp.keyword;
 
 import java.util.*;
 import com.hankcs.hanlp.HanLP;
 import com.hankcs.hanlp.dictionary.stopword.CoreStopWordDictionary;
 import com.hankcs.hanlp.seg.common.Term;
 
-public class TextRankExtract
+
+public class TextRank
 {
-    static final float d = 0.85f;    // 阻尼系数，一般取值为0.85
-    static final int max_iter = 200; //最大迭代次数
-    static final float min_diff = 0.0001f;
-    private static  int nKeyword=5;     //系统提取的关键字的个数
-    private static  int coCurrenceWindow=5; //同现窗口的大小
-    public TextRankExtract()
+    static final float d = 0.85f;           //damping factor, default 0.85
+    static final int max_iter = 200;        //max iteration times
+    static final float min_diff = 0.0001f;  //condition to judge whether recurse or not
+    private static  int nKeyword=5;         //number of keywords to extract,default 5
+    private static  int coCurrenceWindow=3; //size of the co-occur window, default 3
+    
+    // change default parameters
+    public static void setKeywordNumber(int sysKeywordNum)
     {
-      //jdk bug : Exception in thread "main" java.lang.IllegalArgumentException: Comparison method violates its general contract!
-        System.setProperty("java.util.Arrays.useLegacyMergeSort", "true");
+    	nKeyword = sysKeywordNum;
     }
+ 
+    
+    public static void setWindowSize(int window)
+    {
+    	coCurrenceWindow = window;
+    }
+
     
     /**
-     * 根据文章内容，返回排序好的list格式的关键字
-     * @param title           文章标题
-     * @param content         文章内容
-     * @param sysKeywordCount 需要提取的关键字个数
-     * @param window          同现窗口的大小
-     * @return                根据分数排好序的关键字列表
+     * extract keywords in terms of title and content of document
+     * @param title(String): title of document
+     * @param content(String): content of document
+     * @param sysKeywordCount(int): number of keywords to extract,default 5
+     * @param window(int): size of the co-occur window, default 3
+     * @return (List<String>): list of keywords 
      */
-    public static List<String> getKeyword(String title, String content,int sysKeywordCount,int window)
+    public static List<String> getKeyword(String title, String content)
     {
-    	nKeyword = sysKeywordCount;
-    	Map<String, Float> score = TextRankExtract.getWordScore(title, content, window);      
-        //对提取的关键字按照其得分来排序
+    	
+    	Map<String, Float> score = TextRank.getWordScore(title, content); 
+    	
+        //rank keywords in terms of their score
         List<Map.Entry<String, Float>> entryList = new ArrayList<Map.Entry<String, Float>>(score.entrySet());
         Collections.sort(entryList, new Comparator<Map.Entry<String, Float>>()
         {
@@ -45,7 +58,7 @@ public class TextRankExtract
         }
         );
         
-        //System.out.println("对每个词得分排序结果"+entryList);
+        //System.out.println("After sorting: "+entryList);
         
         List<String> sysKeywordList=new ArrayList<String>();
         
@@ -59,37 +72,15 @@ public class TextRankExtract
             }
         }
 
-/**实验结果显示合并后效果更差
-        //根据词的位置对从文中抽取出来的关键词进行合并        
-        for(int i=0;i<unmergedList.size();i++){
-        	String word1=unmergedList.get(i);
-        	String  mergedKeyword=unmergedList.get(i);
-        	for(int j=i;j<unmergedList.size();j++){
-        		String word2=unmergedList.get(j);
-        		if( (wordPosition.get(word1)-wordPosition.get(word2))== 1){
-        			 mergedKeyword=word2+word1;
-        			 unmergedList.remove(j);//删除已经合并的词的权重低的一个
-        		}
-        		else if( (wordPosition.get(word1)-wordPosition.get(word2))== -1){
-        			mergedKeyword=word1+word2;
-        			unmergedList.remove(j);
-        		}
-        		else 
-        			continue;
-        	}
-        	sysKeywordList.add(mergedKeyword);
-        }
-        
- */
-        
+        System.out.print("window:"+coCurrenceWindow+"\nkeywordNum: "+nKeyword);
         return sysKeywordList;
     }
     
     
     /**
-     * 判断某个词是否一个停止词
-     * @param term  需要判断的词语
-     * @return      词语是停止词返回false，否则返回true
+     * judge whether a word belongs to stop words
+     * @param term(Term): word needed to be judged
+     * @return(boolean):  if the word is a stop word,return false;otherwise return true    
      */
     public static boolean shouldInclude(Term term)
 	    {
@@ -97,23 +88,26 @@ public class TextRankExtract
 	    }
  
     
+
     /**
-     * 根据文章标题和内容，返回分词后通过TextRank算法得到的每个词的得分
-     * @param title   文章标题
-     * @param content 文章内容
-     * @param window  同现窗口大小
-     * @return  文章中所有词及其得分
+     * return score of each word after TextRank algorithm
+     * @param title(String): title of document
+     * @param content(String): content of document
+     * @param window(int): size of the co-occur window, default 3
+     * @return (Map<String,Float>):  score of each word
      */
-    public static Map<String,Float> getWordScore(String title, String content,int window)
+    public static Map<String,Float> getWordScore(String title, String content)
      {
-    	 coCurrenceWindow=window;
-   	
-     	//分词并判断词性
-         List<Term> termList = HanLP.segment(title + content);            
-         int count=1; //记录关键字的位置
+    	 	
+     	 //segment text into words
+         List<Term> termList = HanLP.segment(title + content);
+        
+         int count=1;  //position of each word
          Map<String,Integer> wordPosition = new HashMap<String,Integer>();
+         
          List<String> wordList=new ArrayList<String>();
-         //过滤掉停止词
+         
+         //filter stop words
          for (Term t : termList)
          {
              if (shouldInclude(t))
@@ -126,9 +120,9 @@ public class TextRankExtract
                  }
              }
          }
-        //System.out.println("候选关键词列表:"+wordList);
+         //System.out.println("Keyword candidates:"+wordList);
          
-         // 根据coCurrenceWindow大小连接有关系的词语
+         //generate word-graph in terms of size of co-occur window
          Map<String, Set<String>> words = new HashMap<String, Set<String>>();
          Queue<String> que = new LinkedList<String>();
          for (String w : wordList)
@@ -156,11 +150,10 @@ public class TextRankExtract
                      words.get(w2).add(w1);
                  }
              }
-         }
+         }       
+         //System.out.println("word-graph:"+words); //each k,v represents all the words in v point to k 
          
-         //System.out.println("词图模型为:"+words); //每个词和与其有联系的词组成的hashmap，key被value中的值指向
-         
-         // 迭代直到收敛
+         // iterate till recurse
          Map<String, Float> score = new HashMap<String, Float>();
          for (int i = 0; i < max_iter; ++i)
          {
@@ -181,7 +174,8 @@ public class TextRankExtract
                  max_diff = Math.max(max_diff, Math.abs(m.get(key) - (score.get(key) == null ? 1 : score.get(key))));
              }
              score = m;
-             //收敛后就退出
+             
+             //exit once recurse
              if (max_diff <= min_diff) 
              	break;
          }
@@ -189,4 +183,3 @@ public class TextRankExtract
      }
 }
 
-	
